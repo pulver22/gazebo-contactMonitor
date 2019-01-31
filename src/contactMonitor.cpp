408b5e399b0e338e6d2a596eee7fdcdb196758ed
Riccardo Polvara <pulver22@gmail.com>
2019-01-31 10:01:17 +0000

    Auto stash before merge of "master" and "origin/master"

--- src/contactMonitor.cpp
+++ src/contactMonitor.cpp
@@ -29,24 +29,9 @@ void contact_callback(ConstContactsPtr& _msg)
     collision1 = _msg->contact(i).collision1();
     collision2 = _msg->contact(i).collision2();
 
-    contact_data.collision1_name = collision1;
-    contact_data.collision2_name = collision2;
-    contact_data.wrenches.clear();
-    contact_data.contact_positions.clear();
-    contact_data.contact_normals.clear();
-    contact_data.depths.clear();
 
-    // sum up all wrenches for each DOF
-    geometry_msgs::Wrench total_wrench;
-    total_wrench.force.x = 0;
-    total_wrench.force.y = 0;
-    total_wrench.force.z = 0;
-    total_wrench.torque.x = 0;
-    total_wrench.torque.y = 0;
-    total_wrench.torque.z = 0;
-
-    unsigned int contactGroupSize = _msg->contact(i).position_size();
-    for (unsigned int j = 0; j < contactGroupSize; ++j)
+    if (((collision1.find("thorvald_ii") != std::string::npos) ||
+         (collision2.find("thorvald_ii") != std::string::npos)))
     {
       // Get force, torque and rotate into user specified frame.
       // frame_rot is identity if world is used (default for now)
@@ -63,51 +48,84 @@ void contact_callback(ConstContactsPtr& _msg)
               _msg->contact(i).wrench(j).body_1_wrench().torque().y(),
               _msg->contact(i).wrench(j).body_1_wrench().torque().z()));
 
-      // set wrenches
-      geometry_msgs::Wrench wrench;
-      wrench.force.x = force.X();
-      wrench.force.y = force.Y();
-      wrench.force.z = force.Z();
-      wrench.torque.x = torque.X();
-      wrench.torque.y = torque.Y();
-      wrench.torque.z = torque.Z();
-      contact_data.wrenches.push_back(wrench);
+            unsigned int contactGroupSize = _msg->contact(i).position_size();
+            for (unsigned int j = 0; j < contactGroupSize; ++j)
+            {
+              // loop through individual contacts between collision1 and collision2
+              // gzerr << j << "  Position:"
+              //       << contact.position(j).x() << " "
+              //       << contact.position(j).y() << " "
+              //       << contact.position(j).z() << "\n";
+              // gzerr << "   Normal:"
+              //       << contact.normal(j).x() << " "
+              //       << contact.normal(j).y() << " "
+              //       << contact.normal(j).z() << "\n";
+              // gzerr << "   Depth:" << contact.depth(j) << "\n";
 
-      total_wrench.force.x += wrench.force.x;
-      total_wrench.force.y += wrench.force.y;
-      total_wrench.force.z += wrench.force.z;
-      total_wrench.torque.x += wrench.torque.x;
-      total_wrench.torque.y += wrench.torque.y;
-      total_wrench.torque.z += wrench.torque.z;
+              // Get force, torque and rotate into user specified frame.
+              // frame_rot is identity if world is used (default for now)
+              ignition::math::Quaterniond frame_rot;
+              ignition::math::Vector3d frame_pos;
+              ignition::math::Vector3d force =
+                  frame_rot.RotateVectorReverse(ignition::math::Vector3d(
+                      _msg->contact(i).wrench(j).body_1_wrench().force().x(),
+                      _msg->contact(i).wrench(j).body_1_wrench().force().y(),
+                      _msg->contact(i).wrench(j).body_1_wrench().force().z()));
+              ignition::math::Vector3d torque =
+                  frame_rot.RotateVectorReverse(ignition::math::Vector3d(
+                      _msg->contact(i).wrench(j).body_1_wrench().torque().x(),
+                      _msg->contact(i).wrench(j).body_1_wrench().torque().y(),
+                      _msg->contact(i).wrench(j).body_1_wrench().torque().z()));
 
-      // transform contact positions into relative frame
-      // set contact positions
-      ignition::math::Vector3d position = frame_rot.RotateVectorReverse(
-          ignition::math::Vector3d(_msg->contact(i).position(j).x(),
-                                   _msg->contact(i).position(j).y(),
-                                   _msg->contact(i).position(j).z()) -
-          frame_pos);
-      geometry_msgs::Vector3 contact_position;
-      contact_position.x = position.X();
-      contact_position.y = position.Y();
-      contact_position.z = position.Z();
-      contact_data.contact_positions.push_back(contact_position);
+              // set wrenches
+              geometry_msgs::Wrench wrench;
+              wrench.force.x = force.X();
+              wrench.force.y = force.Y();
+              wrench.force.z = force.Z();
+              wrench.torque.x = torque.X();
+              wrench.torque.y = torque.Y();
+              wrench.torque.z = torque.Z();
+              contact_data.wrenches.push_back(wrench);
 
-      // rotate normal into user specified frame.
-      // frame_rot is identity if world is used.
-      ignition::math::Vector3d normal =
-          frame_rot.RotateVectorReverse(ignition::math::Vector3d(
-              _msg->contact(i).normal(j).x(), _msg->contact(i).normal(j).y(),
-              _msg->contact(i).normal(j).z()));
-      // set contact normals
-      geometry_msgs::Vector3 contact_normal;
-      contact_normal.x = normal.X();
-      contact_normal.y = normal.Y();
-      contact_normal.z = normal.Z();
-      contact_data.contact_normals.push_back(contact_normal);
+              total_wrench.force.x += wrench.force.x;
+              total_wrench.force.y += wrench.force.y;
+              total_wrench.force.z += wrench.force.z;
+              total_wrench.torque.x += wrench.torque.x;
+              total_wrench.torque.y += wrench.torque.y;
+              total_wrench.torque.z += wrench.torque.z;
 
-      // set contact depth, interpenetration
-      contact_data.depths.push_back(_msg->contact(i).depth(j));
+              // transform contact positions into relative frame
+              // set contact positions
+              ignition::math::Vector3d position = frame_rot.RotateVectorReverse(
+                  ignition::math::Vector3d(_msg->contact(i).position(j).x(),
+                                           _msg->contact(i).position(j).y(),
+                                           _msg->contact(i).position(j).z()) -
+                  frame_pos);
+              geometry_msgs::Vector3 contact_position;
+              contact_position.x = position.X();
+              contact_position.y = position.Y();
+              contact_position.z = position.Z();
+              contact_data.contact_positions.push_back(contact_position);
+
+              // rotate normal into user specified frame.
+              // frame_rot is identity if world is used.
+              ignition::math::Vector3d normal =
+                  frame_rot.RotateVectorReverse(ignition::math::Vector3d(
+                      _msg->contact(i).normal(j).x(), _msg->contact(i).normal(j).y(),
+                      _msg->contact(i).normal(j).z()));
+              // set contact normals
+              geometry_msgs::Vector3 contact_normal;
+              contact_normal.x = normal.X();
+              contact_normal.y = normal.Y();
+              contact_normal.z = normal.Z();
+              contact_data.contact_normals.push_back(contact_normal);
+
+              // set contact depth, interpenetration
+              contact_data.depths.push_back(_msg->contact(i).depth(j));
+            }
+//            fag_pub.publish(contact_data);
+        }
+
     }
 
     fag_pub.publish(contact_data);
@@ -123,9 +141,9 @@ void contact_callback(ConstContactsPtr& _msg)
       pub.publish(tmstp);
       return;
     }
-  }
+  return;
 }
-
+}
 /////////////////////////////////////////////////
 int main(int _argc, char** _argv)
 {
@@ -133,7 +151,7 @@ int main(int _argc, char** _argv)
   ros::init(_argc, _argv, "contactMonitor");
   ros::NodeHandle nh;
 
-  // read configuration
+  // read coniguration
   ros::param::param<std::string>("~collisions_topic_name",
                                  collisions_topic_name, "/collisions");
   ros::param::param<std::string>("~collision_names_topic_name",
@@ -143,6 +161,8 @@ int main(int _argc, char** _argv)
                                  "robot1");
   ros::param::param<std::string>("~actor_model_name", actor_model_name,
                                  "actor1");
+  ros::param::param<std::string>("~queue_size", queue_size, "5");
+  ros::param::param<std::string>("~loop_rate", loop_rate, "2500");
 
   pub = nh.advertise<std_msgs::Time>(collisions_topic_name, 5);
   fag_pub =
